d2e39bc415d0f51e090689fe7a3296a371eb00a2
Jess <jessjo.324@gmail.com>
2024-09-01 12:57:19 -0500

    Add list of documents already signed in the profile

--- front/lib/user_profile_page.dart
+++ front/lib/user_profile_page.dart
@@ -5,87 +5,157 @@ class UserProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> signedDocuments = [
+      'Contrato_2023.pdf',
+      'Reporte_de_gastos.pdf',
+      'Planificación.docx'
+    ];
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.4,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20),
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.purple[200],
-                  child: const Icon(
-                    Icons.person,
-                    size: 50,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'Perfil de usuario',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple[800],
-                  ),
-                ),
-                const SizedBox(height: 30),
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        UserInfoField(
-                          icon: Icons.person,
-                          label: 'Nombre',
-                          value: 'Jessica Johana',
-                        ),
-                        Divider(),
-                        UserInfoField(
-                          icon: Icons.person_outline,
-                          label: 'Apellidos',
-                          value: 'Ospina',
-                        ),
-                      ],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.purple[200],
+                    child: const Icon(
+                      Icons.person,
+                      size: 50,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        UserInfoField(
-                          icon: Icons.phone,
-                          label: 'Teléfono',
-                          value: '555-555-555',
-                        ),
-                        Divider(),
-                        UserInfoField(
-                          icon: Icons.email,
-                          label: 'Email',
-                          value: 'Jess@gmail.com',
-                        ),
-                      ],
+                  const SizedBox(height: 20),
+                  Text(
+                    'Perfil de usuario',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple[800],
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 30),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          UserInfoField(
+                            icon: Icons.person,
+                            label: 'Nombre',
+                            value: 'Jessica Johana',
+                          ),
+                          Divider(),
+                          UserInfoField(
+                            icon: Icons.person_outline,
+                            label: 'Apellidos',
+                            value: 'Ospina',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          UserInfoField(
+                            icon: Icons.phone,
+                            label: 'Teléfono',
+                            value: '555-555-555',
+                          ),
+                          Divider(),
+                          UserInfoField(
+                            icon: Icons.email,
+                            label: 'Email',
+                            value: 'Jess@gmail.com',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+            const SizedBox(width: 20),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.25,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Documentos firmados',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple[800],
+                        ),
+                      ),
+                      const Divider(),
+                      Expanded(
+                        child: signedDocuments.isNotEmpty
+                            ? ListView.builder(
+                                itemCount: signedDocuments.length,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    leading: Icon(Icons.description,
+                                        color: Colors.purple[800]),
+                                    title: Text(signedDocuments[index]),
+                                  );
+                                },
+                              )
+                            : Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.warning_amber_rounded,
+                                      size: 40,
+                                      color: Colors.grey[600],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      'No hay documentos firmados',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.grey[600],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
